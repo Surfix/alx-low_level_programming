fd11e26a7c4d36d323da927b6b2d872fbd023bbb
surfix <adebisi.naheem@gmail.com>
2022-07-01 14:45:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print size of various types
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	prinf("size of a char: %d bytes\n", (int) sizeof(char));
+	printf("size of an int: %d bytes\n", (int) sizeof(int));
+	printf("size of a long: %d bytes\n", (int) sizeof(long));
+	printf("size of a long long: %d bytes\n", (int) sizeof(long long));
+	printf("size of a float: %d bytes\n", (int) sizeof(float));
+	return (0);
+}
