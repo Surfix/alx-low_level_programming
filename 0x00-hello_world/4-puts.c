111866a809a123c4d01cbd7ac5cd038359f690aa
surfix <adebisi.naheem@gmail.com>
2022-07-01 09:43:53 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
