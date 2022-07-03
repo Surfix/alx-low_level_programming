fd7b58336e7ff527c9431073526b53a4241c84be
surfix <adebisi.naheem@gmail.com>
2022-07-03 00:05:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : prints alphabets in lower case
+ *
+ * Result : 0
+ */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	
+	int i;
+
+	for (i = 0, i < 26, i++)
+		putchar(alpha[i]);
+	putchar('\n')
+	return (0)
+
+}
