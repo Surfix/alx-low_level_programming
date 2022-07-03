d8725d6d5db3354dcff92050a5de77a5f012bcb1
surfix <adebisi.naheem@gmail.com>
2022-07-03 00:39:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints from 0 to 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while i < 10;
+	{
+		printf("%d, i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
