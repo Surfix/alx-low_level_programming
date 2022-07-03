8e34aafa00da0d4332976ea641ddb4b1d96f0578
surfix <adebisi.naheem@gmail.com>
2022-07-03 01:17:08 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: prints lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	       putchar(l);
+	putchar('\n');
+
+	return (0);
+}
+
