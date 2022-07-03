84925d4d238f7ca6ab9f196bf66051900d91e7c1
surfix <adebisi.naheem@gmail.com>
2022-07-03 12:10:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: prints the numbers of base 16 in lower case
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n[] = "0123456789abcdef";
+
+	int i = 0;
+
+	while (i < 16)
+	{
+		putchar(n[i]);
+		i++;
+	}
+	putchar("\n");
+
+	return (0);
+}
