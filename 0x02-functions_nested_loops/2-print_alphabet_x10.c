12d26a8dc2b9f9ee4251fed8076e92230fa69a5e
surfix <adebisi.naheem@gmail.com>
2022-07-05 08:39:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,7 +16,7 @@ void print_alphabet_x10(void)
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
 
 	for (j = 0; j < 10; j++)
-		for (i = 0; i < 26; i++)
+		for (i = 0; i < 27; i++)
 			_putchar(alpha[i]);
 	_putchar('\n');
 }
