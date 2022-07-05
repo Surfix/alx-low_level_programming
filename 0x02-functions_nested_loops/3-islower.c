84d918b011b0aa053dce84532bb88f1b9092cea5
surfix <adebisi.naheem@gmail.com>
2022-07-05 14:47:34 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _islower(int c)
+{
+	int c;
+
+	char lower_alpha = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	int result;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (lower_alpha[i] == c)
+		{
+			result = 1;
+		}
+		else
+		{
+			result = 0;
+		}
+	}
+
+	return (result);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,3 +15,10 @@ void print_alphabet(void);
  */
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
+/**
+ * islower - check for lowercase character
+ *
+ * Return: 1 or 0
+ */
