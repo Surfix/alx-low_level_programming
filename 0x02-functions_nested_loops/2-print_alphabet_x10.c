8a4f1a21820a372e6e21cd4317708c06a93a5cc2
surfix <adebisi.naheem@gmail.com>
2022-07-05 08:36:25 -0700

    modify for new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,11 +13,10 @@ void print_alphabet_x10(void)
 
 	int i;
 
-	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
 
 	for (j = 0; j < 10; j++)
 		for (i = 0; i < 26; i++)
 			_putchar(alpha[i]);
 	_putchar('\n');
-	return;
 }
