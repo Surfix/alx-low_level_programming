987fe2e1a9b627043d4c23eb9ab02723cfee27d3
surfix <adebisi.naheem@gmail.com>
2022-07-09 11:24:24 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line -print straight line
+ *
+ * @n: int
+ *
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+		_putchar('_');
+
+	_putchar('\n');
+}
+
