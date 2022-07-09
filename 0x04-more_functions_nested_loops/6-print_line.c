70d09eeac0f574f4e54c69fabe3730f10fa511cf
surfix <adebisi.naheem@gmail.com>
2022-07-09 11:28:45 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * print_line -print straight line
+ * print_line - prints a straight line
  *
  * @n: int
  *
  * Return: Nothing
  */
 
+
 void print_line(int n)
 {
-	while (n > 0)
-		_putchar('_');
+	int i;
 
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
 	_putchar('\n');
 }
-
