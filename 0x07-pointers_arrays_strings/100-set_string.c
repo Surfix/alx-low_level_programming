0f1c727aa2e76b41b3dd6beb919118c25b976a92
surfix <adebisi.naheem@gmail.com>
2022-07-19 19:46:16 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - set value of string j to k
+ *
+ * @s: pointer to a pointer
+ * @to: char pointer
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
