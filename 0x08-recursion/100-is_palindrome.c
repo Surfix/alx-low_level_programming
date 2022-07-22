ae49f5c4de455b042ecf212d34c0a9b480546b00
surfix <adebisi.naheem@gmail.com>
2022-07-21 19:17:46 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is palindrome
+ *
+ * @s: string pointer
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = strlen(s);
+
+	if (l == 0 || l == 1)
+	{
+		return (1);
+	}
+
+	if (s[0] == s[l - 1])
+	{
+		return (is_palindrome(s + 1));
+	}
+
+	return (0);
+}
