771fb4b3e2c5500e5374c73188e8a39559f7aad8
surfix <adebisi.naheem@gmail.com>
2022-08-02 13:19:12 -0700

    betty style

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+* init_dog- initialize a variable dog
+* @d: struct dog variable
+* @name: char of dog name
+* @age: float of dog age
+* @owner: char of dog owner
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
