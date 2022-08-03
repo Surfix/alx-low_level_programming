62b31dea3b4c0b192fc8ea7ec279969c3ccee720
surfix <adebisi.naheem@gmail.com>
2022-08-02 21:07:38 -0700

    new tasks

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints the detail of struct dog
+ * @d: struct dog
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	char *n = "(nil)";
+
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->age < 0)
+	{
+		printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, n, d->owner);
+		return;
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}  
